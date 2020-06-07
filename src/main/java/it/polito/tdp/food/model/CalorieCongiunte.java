52b5a36cfd37010870a69c6e21c31e7dda2bb08b
elisa <elisa@elisa.home-life.hub>
2020-06-07 22:25:18 +0200

    Soluzione punto 1

--- src/main/java/it/polito/tdp/food/model/CalorieCongiunte.java
+++ src/main/java/it/polito/tdp/food/model/CalorieCongiunte.java
@@ -4,49 +4,59 @@ public class CalorieCongiunte implements Comparable<CalorieCongiunte>{
 
 	Food f1;
 	Food f2;
-	double peso;
+	Double peso;
 	
-	public CalorieCongiunte(Food f1, Food f2, double peso) {
+	
+	public CalorieCongiunte(Food f1, Food f2, Double peso) {
 		super();
 		this.f1 = f1;
 		this.f2 = f2;
 		this.peso = peso;
 	}
 
+
 	public Food getF1() {
 		return f1;
 	}
+
 
 	public void setF1(Food f1) {
 		this.f1 = f1;
 	}
 
+
 	public Food getF2() {
 		return f2;
 	}
+
 
 	public void setF2(Food f2) {
 		this.f2 = f2;
 	}
 
-	public double getPeso() {
+
+	public Double getPeso() {
 		return peso;
 	}
 
-	public void setPeso(double peso) {
+
+	public void setPeso(Double peso) {
 		this.peso = peso;
 	}
+
 
 	@Override
 	public String toString() {
 		return "CalorieCongiunte [f1=" + f1 + ", f2=" + f2 + ", peso=" + peso + "]";
 	}
 
-	//meno = decrescente
+
 	@Override
 	public int compareTo(CalorieCongiunte o) {
-		return -(int) (this.getPeso()-o.getPeso());
+		return -(this.getPeso().compareTo(o.getPeso()));
 	}
+	
+	
 	
 	
 }
